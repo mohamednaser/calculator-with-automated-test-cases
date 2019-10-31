60cad66fc758f780d9d5ed90ca99d229969938a8
Mohamed Naser <nc_m.naser@hotmail.com>
2019-11-01 00:31:29 +0200

    add some basic logic to calculator

---
+++ bin/main.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Calculator
+  def add(param1, param2)
+    param1 += param2it 
+  end
+
+  def minace(param1, param2)
+    false if param2 < param1
+
+    param1 -= param2
+  end
+
+  def multiple(param1, param2)
+    param1 *= param2
+  end
+
+  def divide(param1, param2)
+    param1 / param2
+  end
+end
